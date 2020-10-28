7fb202438030141f673340798388e58574d736e3
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-10-28 14:52:58 +0300

    Add StudentRegistrationActivity and its layout

---
+++ app/src/main/java/com/moringaschool/schoolsystem/ui/StudentRegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.schoolsystem.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.schoolsystem.R;
+
+public class StudentRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_registration);
+    }
+}
