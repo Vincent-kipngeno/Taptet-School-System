f53febf418df0d7ec310c3dcf07713d71849831a
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-10-28 14:32:32 +0300

    Move login and main activity to UI directory

--- app/src/main/java/com/moringaschool/schoolsystem/ui/LoginActivity.java
+++ app/src/main/java/com/moringaschool/schoolsystem/ui/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.moringaschool.schoolsystem;
+package com.moringaschool.schoolsystem.ui;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +23,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.moringaschool.schoolsystem.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
--- app/src/main/java/com/moringaschool/schoolsystem/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/schoolsystem/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.moringaschool.schoolsystem;
+package com.moringaschool.schoolsystem.ui;
 
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +7,7 @@ import android.view.Menu;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.moringaschool.schoolsystem.R;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
