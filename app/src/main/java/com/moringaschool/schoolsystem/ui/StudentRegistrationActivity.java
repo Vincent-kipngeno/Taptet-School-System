f75cc8399e5385543059eeba01dcf0f12ef8ef7d
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-11-01 19:13:48 +0300

    Add radioButtons for classes, add submit button and implement onClickListener to RegistrationActivity

--- app/src/main/java/com/moringaschool/schoolsystem/ui/StudentRegistrationActivity.java
+++ app/src/main/java/com/moringaschool/schoolsystem/ui/StudentRegistrationActivity.java
@@ -3,6 +3,8 @@ package com.moringaschool.schoolsystem.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -12,7 +14,7 @@ import com.moringaschool.schoolsystem.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class StudentRegistrationActivity extends AppCompatActivity {
+public class StudentRegistrationActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.edit_name) EditText mStudentName;
     @BindView(R.id.edit_class) EditText mStudentClass;
     @BindView(R.id.edit_admissionNo) EditText mStudentAdmission;
@@ -26,6 +28,17 @@ public class StudentRegistrationActivity extends AppCompatActivity {
     @BindView(R.id.radio_day) RadioButton mRadioDay;
     @BindView(R.id.radio_border) RadioButton mRadioBoarder;
     @BindView(R.id.radio_sex) RadioGroup mRadioSex;
+    @BindView(R.id.radio_class1) RadioGroup mRadioClass1;
+    @BindView(R.id.radio_class2) RadioGroup mRadioClass2;
+    @BindView(R.id.radio_class3) RadioGroup mRadioClass3;
+    @BindView(R.id.radio_class4) RadioGroup mRadioClass4;
+    @BindView(R.id.radio_class5) RadioGroup mRadioClass5;
+    @BindView(R.id.radio_class6) RadioGroup mRadioClass6;
+    @BindView(R.id.radio_class7) RadioGroup mRadioClass7;
+    @BindView(R.id.radio_class8) RadioGroup mRadioClass8;
+    @BindView(R.id.radio_classPre1) RadioGroup mRadioClassPre1;
+    @BindView(R.id.radio_classPre2) RadioGroup mRadioClassPre2;
+    @BindView(R.id.submit_studentRegistration) Button mSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +46,13 @@ public class StudentRegistrationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_student_registration);
 
         ButterKnife.bind(this);
+
+        mSubmit.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
 
     }
 }
