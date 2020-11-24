60cca7f6acc20a25de7b341aaa1f62854628f2ab
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-11-24 11:30:43 +0300

    Create fee structure table

--- app/src/main/java/com/moringaschool/schoolsystem/ui/AcademicCalendarDetailsActivity.java
+++ app/src/main/java/com/moringaschool/schoolsystem/ui/AcademicCalendarDetailsActivity.java
@@ -1,16 +1,31 @@
 package com.moringaschool.schoolsystem.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.moringaschool.schoolsystem.R;
 
+import butterknife.BindView;
+
 public class AcademicCalendarDetailsActivity extends AppCompatActivity {
+    @BindView(R.id.editTerm1_startDate) TextView mStartDate1;
+    @BindView(R.id.editTerm1_endDate) TextView mEndDate1;
+    @BindView(R.id.feeStructure_gridTerm1) RecyclerView mFeeStructureGrid1;
+
+    private DatabaseReference YearDetailsRef;
+    private FirebaseAuth mAuth;
+    private String currentUserID="";
+    private String academicYearId = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_academic_calendar_details);
+        academicYearId = getIntent().getStringExtra("academicYearId");
     }
 }
