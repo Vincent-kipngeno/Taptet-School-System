4d25584053603029063d4b47086880ab069a2ab3
Vincent-kipngeno <kipngenovi2001@gmail.com>
2021-04-02 17:58:32 +0300

    Add studentuid as a parameter to the FeePaymentFragment instance

--- app/src/main/java/com/moringaschool/schoolsystem/ui/FeePaymentFragment.java
+++ app/src/main/java/com/moringaschool/schoolsystem/ui/FeePaymentFragment.java
@@ -10,40 +10,21 @@ import android.view.ViewGroup;
 
 import com.moringaschool.schoolsystem.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FeePaymentFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FeePaymentFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_STUDENT_UID = "param1";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String mStudentUid;
 
     public FeePaymentFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FeePaymentFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FeePaymentFragment newInstance(String param1, String param2) {
+
+    public static FeePaymentFragment newInstance(String studentUid) {
         FeePaymentFragment fragment = new FeePaymentFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_STUDENT_UID, studentUid);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,8 +33,7 @@ public class FeePaymentFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mStudentUid = getArguments().getString(ARG_STUDENT_UID);
         }
     }
 
