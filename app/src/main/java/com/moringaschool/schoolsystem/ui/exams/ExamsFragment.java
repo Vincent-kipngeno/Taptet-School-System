630391abf3bc2a89feb1804df4a64993648b56d4
Vincent-kipngeno <kipngenovi2001@gmail.com>
2021-04-11 09:47:07 +0300

    Initialize variables and views

--- app/src/main/java/com/moringaschool/schoolsystem/ui/exams/ExamsFragment.java
+++ app/src/main/java/com/moringaschool/schoolsystem/ui/exams/ExamsFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -15,9 +16,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.moringaschool.schoolsystem.R;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ExamsFragment extends Fragment {
 
@@ -25,6 +30,10 @@ public class ExamsFragment extends Fragment {
     @BindView(R.id.academic_years_list_exams) RecyclerView yearsList;
     @BindView(R.id.add_year_button_exams) Button addYear;
     @BindView(R.id.noYears_layout_exams) LinearLayout noYearsLayout;
+
+    private DatabaseReference AcademicYearsRef;
+    private FirebaseAuth mAuth;
+    private String currentUserID="";
 
 
     public static ExamsFragment newInstance() {
@@ -35,6 +44,18 @@ public class ExamsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.exams_fragment, container, false);
+
+        ButterKnife.bind(this,view);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        AcademicYearsRef = FirebaseDatabase.getInstance().getReference().child("Years").child("AcademicYears");
+
+        yearsList.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        addYear.setOnClickListener(this);
+
+        checkIfYearsExist();
 
         return view;
     }
