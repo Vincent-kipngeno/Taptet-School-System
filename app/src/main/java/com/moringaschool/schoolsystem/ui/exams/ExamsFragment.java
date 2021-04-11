7f987436f29f6ce7fc72aa5899e95c39b85dde96
Vincent-kipngeno <kipngenovi2001@gmail.com>
2021-04-11 09:43:47 +0300

    bind views of exams fragment

--- app/src/main/java/com/moringaschool/schoolsystem/ui/exams/ExamsFragment.java
+++ app/src/main/java/com/moringaschool/schoolsystem/ui/exams/ExamsFragment.java
@@ -7,16 +7,25 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.moringaschool.schoolsystem.R;
+
+import butterknife.BindView;
 
 public class ExamsFragment extends Fragment {
 
     private ExamsViewModel mViewModel;
+    @BindView(R.id.academic_years_list_exams) RecyclerView yearsList;
+    @BindView(R.id.add_year_button_exams) Button addYear;
+    @BindView(R.id.noYears_layout_exams) LinearLayout noYearsLayout;
+
 
     public static ExamsFragment newInstance() {
         return new ExamsFragment();
