9d82b2f1a5fd6069fef14d565de843012d44ddbe
Vincent-kipngeno <kipngenovi2001@gmail.com>
2021-04-11 10:11:12 +0300

    Add fillYearsRecyclerView for filling the years layout in examsfragment

--- app/src/main/java/com/moringaschool/schoolsystem/ui/exams/ExamsFragment.java
+++ app/src/main/java/com/moringaschool/schoolsystem/ui/exams/ExamsFragment.java
@@ -2,6 +2,7 @@ package com.moringaschool.schoolsystem.ui.exams;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -17,6 +18,8 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -24,11 +27,14 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.moringaschool.schoolsystem.R;
+import com.moringaschool.schoolsystem.models.AcademicYear;
+import com.moringaschool.schoolsystem.ui.AcademicCalendarDetailsActivity;
+import com.moringaschool.schoolsystem.ui.calendar.CalendarFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ExamsFragment extends Fragment {
+public class ExamsFragment extends Fragment implements View.OnClickListener {
 
     private ExamsViewModel mViewModel;
     @BindView(R.id.academic_years_list_exams) RecyclerView yearsList;
@@ -107,6 +113,53 @@ public class ExamsFragment extends Fragment {
 
             }
         });
+    }
+
+    public void fillYearsRecyclerView() {
+        FirebaseRecyclerOptions<AcademicYear> options =
+                new FirebaseRecyclerOptions.Builder<AcademicYear>()
+                        .setQuery(AcademicYearsRef, AcademicYear.class)
+                        .build();
+
+
+        FirebaseRecyclerAdapter<AcademicYear, ExamsFragment.YearsViewHolder> adapter =
+                new FirebaseRecyclerAdapter<AcademicYear, ExamsFragment.YearsViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull final ExamsFragment.YearsViewHolder holder, int position, @NonNull AcademicYear model)
+                    {
+                        final String academicYearId = getRef(position).getKey();
+
+                        holder.yearName.setText(model.getAcademicYear());
+                        holder.startDate.setText(model.getStartDate());
+                        holder.endDate.setText(model.getEndDate());
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view)
+                            {
+                                Intent yearIntent = new Intent(getContext(), AcademicCalendarDetailsActivity.class);
+                                yearIntent.putExtra("academicYearId", academicYearId);
+                                yearIntent.putExtra("academicYearName", model.getAcademicYear());
+                                startActivity(yearIntent);
+                            }
+                        });
+                    }
+
+                    @NonNull
+                    @Override
+                    public ExamsFragment.YearsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i)
+                    {
+                        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.custom_academic_year_layout, viewGroup, false);
+                        return new ExamsFragment.YearsViewHolder(view);
+                    }
+                };
+
+        yearsList.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 
     public static class  YearsViewHolder extends RecyclerView.ViewHolder
