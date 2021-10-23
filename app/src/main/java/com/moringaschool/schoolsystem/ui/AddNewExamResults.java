8550eaec4d2bb863c069925c8db3ec554a3ac9f4
Vincent-kipngeno <kipngenovi2001@gmail.com>
2021-10-23 17:37:50 +0300

    Implement onItemSelected of Spinner class and also OnIteClickedListener

--- app/src/main/java/com/moringaschool/schoolsystem/ui/AddNewExamResults.java
+++ app/src/main/java/com/moringaschool/schoolsystem/ui/AddNewExamResults.java
@@ -3,12 +3,14 @@ package com.moringaschool.schoolsystem.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import com.moringaschool.schoolsystem.R;
 
-public class AddNewExamResults extends AppCompatActivity {
+public class AddNewExamResults extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
 
     Spinner spinner;
 
@@ -28,5 +30,20 @@ public class AddNewExamResults extends AppCompatActivity {
                 R.array.class_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
