ea67e790bb5cdfb80f077d71ccb98e367ab6fe39
Vincent-kipngeno <kipngenovi2001@gmail.com>
2021-10-23 09:33:11 +0300

    Add code to fill class spinner programatically

--- app/src/main/java/com/moringaschool/schoolsystem/ui/AddNewExamResults.java
+++ app/src/main/java/com/moringaschool/schoolsystem/ui/AddNewExamResults.java
@@ -3,14 +3,30 @@ package com.moringaschool.schoolsystem.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.moringaschool.schoolsystem.R;
 
 public class AddNewExamResults extends AppCompatActivity {
 
+    Spinner spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_exam_results);
+
+        spinner = (Spinner) findViewById(R.id.class_spinner);
+
+        fillClassSpinner();
+
+    }
+
+    public void fillClassSpinner(){
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.class_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
