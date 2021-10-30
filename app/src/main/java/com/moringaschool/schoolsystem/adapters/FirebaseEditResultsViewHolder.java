fe152b1443f565f4f6a09e74b6c252ab054af6b5
Vincent-kipngeno <kipngenovi2001@gmail.com>
2021-10-30 20:03:24 +0300

    Add functionality to save exam result

--- app/src/main/java/com/moringaschool/schoolsystem/adapters/FirebaseEditResultsViewHolder.java
+++ app/src/main/java/com/moringaschool/schoolsystem/adapters/FirebaseEditResultsViewHolder.java
@@ -4,16 +4,21 @@ import android.content.Context;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.moringaschool.schoolsystem.R;
+import com.moringaschool.schoolsystem.models.ExamDetails;
 import com.moringaschool.schoolsystem.models.ExamResult;
 import com.moringaschool.schoolsystem.models.Student;
 
@@ -24,6 +29,17 @@ public class FirebaseEditResultsViewHolder extends RecyclerView.ViewHolder imple
     EditText studentNameText, engEdit, compEdit, engTotEdit, kisEdit, insEdit, kisTotEdit, matEdit, sciEdit, sstEdit, creEdit, sreTotEdit, total;
     Button save, edit;
     Context mContext;
+    ExamResult result;
+
+    DatabaseReference DatabaseRef = FirebaseDatabase.getInstance().getReference();
+
+    DatabaseReference ExamsRef = DatabaseRef.child("Exams");
+    DatabaseReference OverallExamsRef = ExamsRef.child("OverallExams");
+    DatabaseReference StudentsExamsRef = ExamsRef.child("StudentsExams");
+    DatabaseReference ClassExamsRef = ExamsRef.child("ClassExams");
+    DatabaseReference YearDetailsRef = DatabaseRef.child("Years");
+    DatabaseReference UsersRef = DatabaseRef.child("Users");
+    DatabaseReference currentAcademicYearRef = YearDetailsRef.child("CurrentAcademicYear");
 
     public FirebaseEditResultsViewHolder(@NonNull View itemView) {
         super(itemView);
@@ -51,7 +67,7 @@ public class FirebaseEditResultsViewHolder extends RecyclerView.ViewHolder imple
     }
 
     public void bindRestaurant(ExamResult result, Boolean isEdit) {
-        DatabaseReference UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        this.result = result;
 
         if (isEdit){
             save.setVisibility(View.GONE);
@@ -124,10 +140,111 @@ public class FirebaseEditResultsViewHolder extends RecyclerView.ViewHolder imple
     @Override
     public void onClick(View v) {
         if (v == save){
-            //examId =
+            String eng = engEdit.getText().toString();
+            String comp = compEdit.getText().toString();
+            String kis = kisEdit.getText().toString();
+            String ins = insEdit.getText().toString();
+            String mat = matEdit.getText().toString();
+            String sci = matEdit.getText().toString();
+            String sst = sstEdit.getText().toString();
+            String cre = creEdit.getText().toString();
+
+            boolean isValid = validateInputs(eng, comp, kis, ins, mat, sci, sst, cre);
+
+            if (!isValid){
+                validateInputs(eng, comp, kis, ins, mat, sci, sst, cre);
+            }
+            else{
+
+                currentAcademicYearRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot)
+                    {
+                        if (dataSnapshot.exists())
+                        {
+                            String currentAcademicYearId = dataSnapshot.child("AcademicYearId").getValue().toString();
+                            String currentAcademicTerm = dataSnapshot.child("Term").getValue().toString();
+
+                            DatabaseReference ExamsRef = OverallExamsRef.child(currentAcademicYearId).child(currentAcademicTerm).child("ExamResults");
+
+                            DatabaseReference examRef = ExamsRef.push();
+                            String examId = examRef.getKey();
+
+                            ExamResult newResult = new ExamResult(result.getStudentClass(), result.getStudentId(), examId, result.getExamTypeId(), result.getDateDone(), Integer.parseInt(eng),  Integer.parseInt(comp), Integer.parseInt(kis), Integer.parseInt(ins), Integer.parseInt(mat), Integer.parseInt(sci), Integer.parseInt(sst), Integer.parseInt(cre));
+
+                            ExamsRef.child(examId).setValue(newResult).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful())
+                                    {
+                                        Toast.makeText(mContext, "Exam result added Successfully...", Toast.LENGTH_SHORT).show();
+
+                                        save.setVisibility(View.GONE);
+                                        edit.setVisibility(View.VISIBLE);
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(mContext, "Error adding exam result. Retry .", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+
+                        }
+                        else {
+                            Toast.makeText(mContext, "Kindly Create and Start academic year First", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
         }
         else if (v == edit){
 
+        }
+
+    }
+
+    public boolean validateInputs(String eng, String comp, String kis, String ins, String mat, String sci, String sst, String cre){
+
+        if (eng==null || eng.trim().isEmpty()){
+            engEdit.setError("Required Field");
+            return false;
+        }
+        else if (comp==null || comp.trim().isEmpty()){
+            compEdit.setError("Required Field");
+            return false;
+        }
+        else if (kis==null || kis.trim().isEmpty()){
+            kisEdit.setError("Required Field");
+            return false;
+        }
+        else if (ins==null || ins.trim().isEmpty()){
+            insEdit.setError("Required Field");
+            return false;
+        }
+        else if (mat==null || mat.trim().isEmpty()){
+            matEdit.setError("Required Field");
+            return false;
+        }
+        else if (sci==null || sci.trim().isEmpty()){
+            sciEdit.setError("Required Field");
+            return false;
+        }
+        else if (sst==null || sst.trim().isEmpty()){
+            sstEdit.setError("Required Field");
+            return false;
+        }
+        else if (cre==null || cre.trim().isEmpty()){
+            creEdit.setError("Required Field");
+            return false;
+        }
+        else{
+            return true;
         }
 
     }
